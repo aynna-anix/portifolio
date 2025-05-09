286ccb1d1d6e4bcbdf5eeff479b5d163d1e2afb9
Aynna <aynavalentina@gmail.com>
2025-05-09 12:41:46 +0000

    começo do js

--- exercicio 1/script.js
+++ exercicio 1/script.js
@@ -3,7 +3,11 @@ let num1=0;
 let num2=0;
 
 
-num1=window.prompt('digite um numero:');
-num2=window.prompt('digite um numero:');
+num1=Number(window.prompt('digite um numero:'));
+num2=Number(window.prompt('digite um numero:'));
+
+let soma =num1+num2;
+
+alert(soma)
 
 }
