40bb184955a2cdf0824858c6c2555f39ba846ccd
Aynna <aynavalentina@gmail.com>
2025-06-29 16:51:25 +0000

    12 ex

--- ex12/script12.js
+++ ex12/script12.js
@@ -1,4 +1,24 @@
-function gerartabuada()
-//pede ao usuario para digitar um numero
-const input=prompt
-const numero=prompt
+function gerartabuada() {
+    // Pede ao usuário para digitar um número
+    const input = prompt('Digite um número para ver sua tabuada');
+    const numero = Number(input);
+    const ResultadoElement = document.getElementById('result');
+
+    // Verificar se o valor fornecido é um número válido 
+    if (isNaN(numero)) {
+        ResultadoElement.textContent = "Por favor, digite um número válido!";
+        return;
+    }
+
+    let contador = 1;
+    const resultado = [];
+
+    // Loop while para gerar a tabuada  
+    while (contador <= 10) {
+        resultado.push(`${numero} x ${contador} = ${numero * contador}`);
+        contador++;
+    }
+
+
+
+}
